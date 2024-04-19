6324f31f14cf6e31ece505659b6cb93012f01261
Tomáš Dračka <tomidracka@gmail.com>
2024-04-19 13:10:16 +0200

    Initial

---
+++ backend/backend/Program.cs
@@ -0,0 +1,18 @@
+﻿using Fleck;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var server = new WebSocketServer("ws://0.0.0.0:8181");
+        server.Start(socket =>
+        {
+            socket.OnOpen = () => Console.WriteLine("Open!");
+            socket.OnClose = () => Console.WriteLine("Close!");
+            socket.OnMessage = message => socket.Send(message); 
+        });
+
+        Console.WriteLine("Server started on ws://0.0.0.0:8181");
+        Console.ReadLine(); 
+    }
+}
