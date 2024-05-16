88e9afe368a92514996c7fd8ab90ca780bbc466b
Martin Grulyó <106872157+mrtng1@users.noreply.github.com>
2024-05-16 16:18:24 +0200

    basic about us translation

--- frontend/src/app/components/about/about.component.ts
+++ frontend/src/app/components/about/about.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import {TranslateModule, TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  constructor(private translate: TranslateService) {
+    // Set the default language to English
+    this.translate.setDefaultLang('en');
 
+    // Optionally set the initial language based on user preference or browser settings
+    this.translate.use('en');
+  }
+
+  // Function to switch languages
+  switchLanguage(lang: string) {
+    this.translate.use(lang);
+  }
 }
