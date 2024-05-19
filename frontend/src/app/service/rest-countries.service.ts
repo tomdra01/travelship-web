5381cf3b94fa084808d47b29b92eadc42fdde6b0
Martin Grulyó <106872157+mrtng1@users.noreply.github.com>
2024-05-19 17:50:01 +0200

    design

--- frontend/src/app/service/rest-countries.service.ts
+++ frontend/src/app/service/rest-countries.service.ts
@@ -13,14 +13,13 @@ export class RestCountriesService {
 
   searchCountries(query: string): Observable<string[]> {
     if (!query.trim()) {
-      // if not search term, return empty country array.
       return of([]);
     }
     return this.http.get<any[]>(`${this.apiUrl}/${query}?fields=name`).pipe(
       map(countries => countries.map(country => country.name.common)),
       catchError(error => {
         console.error('Failed to fetch countries:', error);
-        return of([]); // Return an empty array on error
+        return of([]);
       })
     );
   }
