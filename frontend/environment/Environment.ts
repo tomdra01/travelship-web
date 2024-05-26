f536c5643b49b9080703fad03a3fc46c6bc9bedf
Martin Grulyó <106872157+mrtng1@users.noreply.github.com>
2024-05-27 00:15:04 +0200

    traefik3

--- frontend/environment/Environment.ts
+++ frontend/environment/Environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   //httpUrl: "http://localhost:5181",
   //websocketUrl: "ws://localhost:8181",
 
-  httpUrl: "http://164.68.109.76",
+  httpUrl: "http://164.68.109.76:5181",
   websocketUrl: "ws://164.68.109.76:8181"
 };
 
