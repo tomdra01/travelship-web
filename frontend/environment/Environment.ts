9ea214cc7f93bc90548d4491b7c3c96c585126e6
Martin Grulyó <106872157+mrtng1@users.noreply.github.com>
2024-05-26 18:36:37 +0200

    change of backend urls

--- frontend/environment/Environment.ts
+++ frontend/environment/Environment.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
-  httpUrl: "http://localhost:5181",
-  websocketUrl: "ws://localhost:8181"
+  //httpUrl: "http://localhost:5181",
+  //websocketUrl: "ws://localhost:8181",
+
+  httpUrl: "http://httpapi:8070",
+  websocketUrl: "ws://wsapi:8181"
 };
