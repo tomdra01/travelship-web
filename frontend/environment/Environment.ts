1336ccbea24069757a1a18476e50650666858490
Martin Grulyó <106872157+mrtng1@users.noreply.github.com>
2024-05-26 23:54:12 +0200

    remove port from env frontend

--- frontend/environment/Environment.ts
+++ frontend/environment/Environment.ts
@@ -1,8 +1,9 @@
 export const environment = {
-  production: true,
+  //production: true,
   //httpUrl: "http://localhost:5181",
   //websocketUrl: "ws://localhost:8181",
 
-  httpUrl: "http://164.68.109.76:5181",
-  websocketUrl: "ws://164.68.109.76:8181"
+  httpUrl: "http://164.68.109.76",
+  websocketUrl: "ws://164.68.109.76"
 };
+
