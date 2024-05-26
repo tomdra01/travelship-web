aa3a4ef490760847a58d0d7efd2777da9ab3b78f
Martin Grulyó <106872157+mrtng1@users.noreply.github.com>
2024-05-27 01:53:58 +0200

    hashlocation strategy appmodule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
+import {CommonModule, HashLocationStrategy, LocationStrategy} from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClient, HttpClientModule} from "@angular/common/http";
@@ -47,6 +47,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     })
   ],
   providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
