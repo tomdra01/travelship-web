bb932403e449fba5aa5002a23b19a54305fffd4e
Martin Grulyó <106872157+mrtng1@users.noreply.github.com>
2024-05-26 20:48:46 +0200

    env change

--- frontend/environment/Environment.ts
+++ frontend/environment/Environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   //httpUrl: "http://localhost:5181",
   //websocketUrl: "ws://localhost:8181",
 
-  httpUrl: "http://httpapi:8070",
-  websocketUrl: "ws://wsapi:8181"
+  httpUrl: "http://localhost:8070",
+  websocketUrl: "ws://localhost:8181"
 };
