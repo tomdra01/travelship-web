21d9375f800547f28de75683ea7221613ab0562e
Martin Grulyó <106872157+mrtng1@users.noreply.github.com>
2024-05-29 01:36:27 +0200

    k6 integration into main pipeline

--- frontend/k6/spiketest.js
+++ frontend/k6/spiketest.js
@@ -3,13 +3,13 @@ import { sleep, check } from 'k6';
 
 export let options = {
     stages: [
-        { duration: '5s', target: 150 }, // 150 users for 5 seconds
-        { duration: '6s', target: 300 }, // 300 users for 6 seconds
-        { duration: '8s', target: 500 }, // 500 users for 8 seconds
+        { duration: '5s', target: 400 }, // 400 users for 5 seconds
+        { duration: '7s', target: 750 }, // 750 users for 7 seconds
+        { duration: '6s', target: 1000 }, // 1200 users for 6 seconds
         { duration: '15s', target: 0 }, // ramp-down to 0 users
     ],
     thresholds: {
-        'http_req_duration': ['p(99)<1500'], // 99% of requests must complete below 1.5s
+        'http_req_duration': ['p(93)<3000'], // 95% of requests must complete below 3s
     },
 };
 
