2dada6454e00f001a6c7337282b45dd3486868ea
Martin Grulyó <106872157+mrtng1@users.noreply.github.com>
2024-05-29 01:18:14 +0200

    change in load test intensity

--- frontend/k6/loadtest.js
+++ frontend/k6/loadtest.js
@@ -2,10 +2,10 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export let options = {
-    vus:1000, // 1000 virtual users
+    vus:800, // 800 virtual users
     duration: '1m',
     thresholds: {
-        'http_req_duration': ['p(90)<200'], // 90% of requests must complete below 200ms
+        'http_req_duration': ['p(90)<225'], // 90% of requests must complete below 225ms
     },
 };
 
